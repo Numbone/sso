ca17e7884dfff3c8d8c62d0c6d5421fa80d11a5f
Numbone <nurlybekqwerty@gmail.com>
2025-11-02 18:48:37 +0500

    feat: loggin service

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,5 +1,50 @@
-package sso
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/Numbone/sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
+	envDev   = "dev"
+)
 
 func main() {
+	cfg := config.MustLoad()
 
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application",
+		slog.String("env", cfg.Env),
+		slog.Any("config", cfg),
+		slog.Int("port", cfg.GRPC.Port))
+
+	log.Debug("debug message")
+
+	log.Error("error message")
+
+	log.Warn("warn message")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,14 +1,51 @@
 package config
 
-import "time"
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
 
 type Config struct {
-	Env         string     `yaml:"env" env-default:"local"`
-	StoragePath string     `yaml:"storage_path" env-required:"true"`
-	GRPC        GRPCConfig `yaml:"grpc"`
+	Env         string        `yaml:"env" env-default:"local"`
+	StoragePath string        `yaml:"storage_path" env-required:"true"`
+	TokenTTL    time.Duration `yaml:"token_ttl" env-required:"1h"`
+	GRPC        GRPCConfig    `yaml:"grpc"`
 }
 
 type GRPCConfig struct {
-	Port    string        `yaml:"port"`
+	Port    int           `yaml:"port"`
 	Timeout time.Duration `yaml:"timeout"`
+}
+
+func MustLoad() *Config {
+	path := fetchConfigPath()
+	if path == "" {
+		panic("config file path is empty")
+	}
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config file not found: " + path)
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(path, &cfg)
+	if err != nil {
+		panic("failed to read config: " + err.Error())
+	}
+	return &cfg
+}
+
+func fetchConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "", "config path")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+	return res
 }
